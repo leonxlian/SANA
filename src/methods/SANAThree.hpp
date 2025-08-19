74269c541f0e9387acda4a28fc7b4e0f15ac9c4b
Marcus Longo <134153953+malongo03@users.noreply.github.com>
2025-08-18 20:01:40 -0400

    Sana3.1 (#176)
    * Rebasing to resolve conflicts
    
    * Fixed a threading bug by replacing a bad wait system with an std function.
    
    * Fixed alignment bug: locked holes were not being properly locked or unlocked in SANAThree::chooseNextRequest()
    
    * I will never use vector<bool> again.
    
    * Bug in trackProgress fixed
    
    * Revert "A massive refactor of SANA, but not extensively tested. Also, I got rid of any bad carriage returns still lingering around."
    
    This reverts commit 2c703df9 for all whitespace only changes
    
    * Minor fixes
    
    * A massive refactor of SANA, but not extensively tested. Also, I got rid of any bad carriage returns still lingering around.
    
    * Fixed alignment bug: locked holes were not being properly locked or unlocked in SANAThree::chooseNextRequest()
    
    * I will never use vector<bool> again.
    
    * Bug in trackProgress fixed
    
    * Revert "A massive refactor of SANA, but not extensively tested. Also, I got rid of any bad carriage returns still lingering around."
    
    This reverts commit 2c703df9 for all whitespace only changes
    
    * Shiny new request system with way too much math to have gotten it right on the first go around.
    
    * Minor fix
    
    * For better SANA comparison
    
    * Thread Number and Legacy SANA added as commandline compile arguments
    
    * Testing a new system for letting threads receive requests directly rather than requiring the main thread to process them.
    
    * Undefined behavior caused mutex weirdness. Here is a fix.
    
    * Some minor optimizations
    
    ---------
    
    Co-authored-by: malonog03 <mlonog2021@gmail.com>

--- src/methods/SANAThree.hpp
+++ src/methods/SANAThree.hpp
@@ -19,18 +19,16 @@
 using namespace std;
 
 // Big Picture TODO list by priority:
-// 0.) Add thread number as a command line argument
-// 1.) Profile this new version and find if the main thread or the daughters are the bottleneck
+// 1.) Add thread number as a command line argument
 // 2.) Refactor and cleanly implement the stationary node system from 2.0
 // 3.) Fix happy batches system
-// 4.) Refactor the node color system
-// 5.) Figure out how to better track the statistics of the SA process without cluttering up the
+// 4.) Figure out how to better track the statistics of the SA process without cluttering up the
 // SANA class like in the 2.0
-// 6.) Goldilocks functionality
-// 7.) SANAWrapper retired
-// 8.) Better thread error detection and/or experiment with more restrictive locking schemes
-// 9.) Individual node KE system
-// 10.) Multi-pairwise SANA reimplemented
+// 5.) Goldilocks functionality
+// 6.) SANAWrapper retired
+// 7.) Better thread error detection and/or experiment with more restrictive locking schemes
+// 8.) Individual node KE system
+// 9.) Multi-pairwise SANA reimplemented
 // If you have ideas for any of this, my element is @malongo:matrix.org
 // -Marcus
 
@@ -64,24 +62,28 @@ private:
     struct changeRequest {
         // Request properties
         const bool twoPegs;
+
         const unsigned peg1;
         const unsigned peg2;
         const unsigned hole1;
         const unsigned hole2;
-        const unsigned activeColorSANA;
-        const unsigned unoccupiedHole2ID;
+
+        const unsigned peg1colorID;
+        const unsigned peg2colorID;
+        const unsigned hole2unassignedID;
+
+        const unsigned color;
 
         // Request output
         double energyInc;
 
-        // Constructor to initialize all members
-        changeRequest(bool twoPegs, unsigned peg1, unsigned peg2,
-                      unsigned hole1, unsigned hole2, unsigned activeColorSANA,
-                      unsigned unoccupiedHole2ID)
-            : twoPegs(twoPegs), peg1(peg1), peg2(peg2),
-              hole1(hole1), hole2(hole2), activeColorSANA(activeColorSANA),
-              unoccupiedHole2ID(unoccupiedHole2ID), energyInc(0.0) {
-            energyInc = 0.0;
+        changeRequest(bool two_pegs, unsigned peg1, unsigned peg2, unsigned hole1, unsigned hole2,
+        unsigned peg1colorID, unsigned peg2colorID, unsigned hole2unassignedID,
+        unsigned colorID, double energyInc):
+            twoPegs(two_pegs), peg1(peg1), peg2(peg2), hole1(hole1), hole2(hole2),
+            peg1colorID(peg1colorID), peg2colorID(peg2colorID), hole2unassignedID(hole2unassignedID),
+            color(colorID) {
+            this->energyInc = energyInc;
         }
     };
 
@@ -106,31 +108,30 @@ private:
         // These are the proper getters and setters for CalculatorHandler. Please use this, minding
         // the below comment.
         // -Marcus
-        void submitRequest(changeRequest input);
-        changeRequest extractRequest();
+        batchOutput collectBatch(double temperature);
 
-        // This function is be considered PRIVATE, and hence the underscore. It is public ONLY for a
-        // single use in _singleThreadBatch in the main class because we have no threads to spare.
-        // Otherwise, this function should never be accessed directly by SANAThree unless you know
-        // what you are doing or have consulted me.
-        // TODO: make this a friend function
-        void _assessChange(changeRequest &input) const{
-            if (input.twoPegs) _assessSwap(input);
-            else _assessMove(input);
-        }
     private:
         bool _calculatorsOn;
-        condition_variable requestSubmitted;
+
         condition_variable requestProcessed;
+        condition_variable startBatch;
         const unsigned _extraThreads;
-        int _requestBalance;
+
+        double temperature;
+        double totalEnergy;
+        double totalPBad;
+
+        unsigned long long _inputRequests;
+        unsigned long long _outputRequests;
+
         SANAThree &_parent;
-        mutex _scoringQueueMutex;
-        mutex _decisionQueueMutex;
-        queue<changeRequest> _scoringQueue;
-        queue<changeRequest> _decisionQueue;
+        mutex _requestSystem;
         vector<thread> _threadVector;
         void _mainLoop();
+        void _assessChange(changeRequest& currentRequest) const {
+            if (currentRequest.twoPegs) _assessSwap(currentRequest);
+            else _assessMove(currentRequest);
+        }
         void _assessMove(changeRequest &input) const; // One pin
         void _assessSwap(changeRequest &input) const; // Two pins
     };
@@ -140,6 +141,7 @@ private:
     const double tolerance;
     const unsigned long long maxSeconds;
     const unsigned long long maxIterations;
+    const unsigned long long batchSize;
     const unsigned threadNumber;
     const MeasureCombination *const MC;
     const Alignment startingAlignment; // Give an empty alignment for a scramble
@@ -157,37 +159,34 @@ private:
     void initDataStructures();
 
     // Main run function and variables
-    vector<unsigned> alignment;
+    Alignment alignment;
     double currentScore;
     void runIterations(CalculatorHandler &threadPool);
     void runConfidenceIntervals(CalculatorHandler &threadPool);
     void runHillClimbing(CalculatorHandler &threadPool);
 
     void scramble();
-    batchOutput collectBatch(CalculatorHandler &threadPool, const double temperature) {
-        if (threadNumber == 1)
-            return _singleThreadBatch(threadPool, temperature);
-        return _multiThreadBatch(threadPool, temperature);
-    }
-    batchOutput _singleThreadBatch(CalculatorHandler &threadPool, double temperature);
-    batchOutput _multiThreadBatch(CalculatorHandler &threadPool, double temperature);
 
     // THE REQUEST SYSTEM
 
-    mt19937 generator; // rng
+    mt19937_64 generator; // rng
     uniform_real_distribution<> randomReal;
 
-    // To mark holes as off limits for swaps or changes. Vector<bool> is too slow, so we shall
-    // sacrifice space for speed.
-    vector<char> lockedHoles;
+    vector<vector<unsigned>> colorUnassignedNodes;
 
-    // This is not mine, which is probably why I wish to rewrite it, lol. -Marcus
-    vector<double> actColToAccumProbCutpoint;
-    vector<double> actColToChangeProb;
-    vector<uint> actColToG1ColId;
-    vector<vector<uint>> actColToUnassignedG2Nodes;
-    vector<uint> g2NodeToActColId; //data structure used to initialize actColToUnassignedG2Nodes
-    uint INVALID_ACTIVE_COLOR_ID; //arbitrary value bigger than any valid active color id
+    // Keeps track of the total number of alignments, swaps, and moves we have access to as changes
+    uint64_t numAdjacentAlignments;
+    uint64_t numSwaps;
+    vector<uint64_t> swapsPerColor;
+    vector<uint64_t> movesPerColor;
+
+    // Keeps track of how many unlocked pegs and unassigned holes per colorID that we have access to
+    vector<unsigned> pegsPerColor;
+    vector<unsigned> unassignedHolesPerColor;
+
+    // Keeps track of which pegs and holes we have locked (if we have threads) per color:
+    vector<set<unsigned>> lockedPegs;
+    vector<set<unsigned>> lockedHoles;
 
     changeRequest chooseNextRequest();
     void implementLastRequest(double pBad, const changeRequest &input);
